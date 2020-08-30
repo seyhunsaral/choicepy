d24d7c01d4a40033e6aa267cf1c6cd5fefb959d4
ali seyhun <saral@posteo.de>
2020-08-30 23:06:52 +0200

    initiate tests

---
+++ tests/test_rules.py
@@ -0,0 +1,9 @@
+import choicepy
+
+my_profile = choicepy.Profile()
+
+my_profile.set_voters([["a","b","c"],["a","c","b"],["c","b","a"]])
+
+
+def test_plurality():
+    assert my_profile.plurality() == ['a']
