5e9ecf7e66efefe3ad25e7efbf0bd622d14602e7
ali seyhun <saral@posteo.de>
2020-08-30 22:13:14 +0200

    add folders and gitignore

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from os import path
+from io import open
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='choicepy',
+    python_requires='>3.6.0',
+    version='0.0.1a',
+	packages=['choicepy'],
+    url='https://github.com/seyhunsaral/choicepy',
+    license='MIT License',
+    author='Annika Hennes, Ali Seyhun Saral',
+    author_email='hennes@coll.mpg.de, saral@coll.mpg.de',
+    description='A package for social choice and voting',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown', 
+	install_requires=['numpy>=1.19.1'],
+    zip_safe=False
+)
